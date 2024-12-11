0eca89fd1d27aef307e8eb0dd391e81046871391
陈沛闻 <chenpeiwen@shenlintech.com>
2024-12-11 16:09:54 +0800

    配置文件上传

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import "./index.css";
 import { TonConnectUIProvider } from "@tonconnect/ui-react";
 
 const manifestUrl =
-  "https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json";
+  "https://https://pew20.github.io/ton-client/public/tonconnect-manifest.json";
 createRoot(document.getElementById("root")!).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
     <App />
